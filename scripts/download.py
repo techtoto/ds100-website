6251a3368be61c872f1ac2b9d917928dc1475352
Marie Ramlow <me@nycode.dev>
2024-10-20 19:38:26 +0200

    add xlsx download script

---
+++ scripts/download.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from bs4 import BeautifulSoup
+from requests import get
+
+URL = "https://www.dbinfrago.com/web/schienennetz/betrieb/allgemeine-betriebsinformationen/betriebsstellen-12592996"
+headers = { "User-Agent": "https://github.com/techtoto/ds100-website" }
+
+text = get(url=URL, headers=headers).text
+soup = BeautifulSoup(text, features="html.parser")
+
+file_url = soup.find(name="a", attrs={ "title": "Übersicht der Betriebsstellen und deren Abkürzungen"}).attrs["href"]
+
+response = get(url=f"https://www.dbinfrago.com/{file_url}", headers=headers)
+with open("Download-betriebsstellen-data.xlsx", "wb") as f:
+    f.write(response.content)
