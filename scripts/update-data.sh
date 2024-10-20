96e2b945f15ba7d28f1906ede7a191dd4a912dda
Marie Ramlow <me@nycode.dev>
2024-10-20 19:38:26 +0200

    add update ci script

---
+++ scripts/update-data.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# This script updates the ril100 data source
+
+echo Downloading data from Deutsche Bahn
+./scripts/download.py
+echo Converting xlsx to csv
+./scripts/convert.nu
+
+# check for changes
+if [[ `git status --porcelain` ]]; then
+  git switch -c update-ril100
+  git commit --all --message "Update ril100 data"
+  git push --force
+
+  if [[ -z "$(gh pr list --head "update-ril100" --state open --json url --jq .[].url)" ]]; then
+    gh pr create \
+      --fill \
+      --reviewer techtoto \
+      --base update-ril100
+  fi
+fi
