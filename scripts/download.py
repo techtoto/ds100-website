7ffc2300fa7fd9dc0f2986f3a9642795c35c525d
Marie Ramlow <me@nycode.dev>
2025-01-13 12:07:05 +0100

    scripts/download.py: fix downloading

--- scripts/download.py
+++ scripts/download.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from bs4 import BeautifulSoup
 from requests import get
+import re
 
 URL = "https://www.dbinfrago.com/web/schienennetz/betrieb/allgemeine-betriebsinformationen/betriebsstellen-12592996"
 headers = { "User-Agent": "https://github.com/techtoto/ds100-website" }
@@ -8,7 +9,7 @@ headers = { "User-Agent": "https://github.com/techtoto/ds100-website" }
 text = get(url=URL, headers=headers).text
 soup = BeautifulSoup(text, features="html.parser")
 
-file_url = soup.find(name="a", attrs={ "title": "Übersicht der Betriebsstellen und deren Abkürzungen"}).attrs["href"]
+file_url = soup.find(name="a", href=re.compile("Download-betriebsstellen-data\\.xlsx$")).attrs["href"]
 
 response = get(url=f"https://www.dbinfrago.com/{file_url}", headers=headers)
 with open("Download-betriebsstellen-data.xlsx", "wb") as f:
